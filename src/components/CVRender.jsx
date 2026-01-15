9d3be6c10c939f0a8f82d616abcd6ee01d98f3a9
Jasper Alblas <just_jasper@hotmail.com>
2026-01-15 21:16:47 +0100

    Adds general info to renderer

--- src/components/CVRender.jsx
+++ src/components/CVRender.jsx
@@ -1,10 +1,21 @@
+import "../styles/CVRender.css";
+
 export default function CVRender({ name, telephone, email }) {
   return (
     <div id="render">
       <h2>CV</h2>
-      <h3>{name}</h3>
-      <h3>{telephone}</h3>
-      <h3>{email}</h3>
+      <div className="general">
+        <h2>General Section</h2>
+        <p>
+          <b>Name:</b> {name}
+        </p>
+        <p>
+          <b>Telephone:</b> {telephone}
+        </p>
+        <p>
+          <b>Email:</b> {email}
+        </p>
+      </div>
     </div>
   );
 }
